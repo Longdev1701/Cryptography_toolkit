cb68062e0ffcdf283955d4a4c93237235f1c8cc3
Longdev1701 <n23dccn035@student.ptithcm.edu.vn>
2026-04-07 23:56:32 +0700

    Demo DES

--- symmetric.py
+++ symmetric.py
@@ -1,5 +1,36 @@
-from Crypto.Cipher import DES, DES3, AES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
-import base64
+from Crypto.Cipher import DES
+from utils import generate_key,pad_data,unpad_data
+
+class des:
+    def encryption(data, key):
+        haveKey = True
+        key = key.encode()
+        if(key == ""):
+            key = generate_key(8)
+            haveKey = False
+
+        cipher = DES.new(key, DES.MODE_CBC)
+        iv = cipher.iv
+
+        padded_data = pad_data(data.encode(),DES.block_size)
+        cipher_text = cipher.encrypt(padded_data)
+
+        return iv + cipher_text + ((b" " + key) if haveKey is not True else b"")
+    
+    def decryption(data,key,iv):
+        decipher = DES.new(key, DES.MODE_CBC, iv)
+        original_data = unpad_data(decipher.decrypt(data),DES.block_size)
+
+        return original_data
+
+
+a = des.encryption("Longlatoi1701@","longlato")
+print(a)
+
+#cipher_text, key = a.split(b" ")
+cipher_text = a[8:]
+iv = a[:8]
+
+b = des.decryption(cipher_text,"longlato".encode(), iv)
+print(b)
 
