bc0b2d150d59af165307effbec45aaa445941234
Longdev1701 <n23dccn035@student.ptithcm.edu.vn>
2026-04-08 08:31:52 +0700

    delete temp code

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-a = des.encryption("Longlatoi1701@","longlato")
-# print(a)
-
-# #cipher_text, key = a.split(b" ")
-# cipher_text = a[8:]
-# iv = a[:8]
-
-# b = des.decryption(cipher_text,"longlato".encode(), iv)
-# print(b)
