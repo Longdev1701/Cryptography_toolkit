1facbdad34d33f9733c9dcd9aee33ccec717ab07
Quach Nam Luong <103545178+QuachNamLuong@users.noreply.github.com>
2026-05-05 07:38:55 +0700

    add hash functions

--- hash.py
+++ hash.py
@@ -1,1 +1,18 @@
 import hashlib
+
+def bam_md5(chuoi_vao):
+    try:
+        b = chuoi_vao.encode('utf-8')
+        h = hashlib.md5(b)
+        return h.hexdigest()
+    except Exception as e:
+        return f"[!] Hashing error: {e}"
+
+def bam_sha256(chuoi_vao):
+    try:
+        b = chuoi_vao.encode('utf-8')
+        h = hashlib.sha256(b)
+        return h.hexdigest()
+    except Exception as e:
+        return f"[!] Hashing error: {e}"
+
